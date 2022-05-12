11d46d5c0ffc11dd711b159b2d0f81a7c1be2a0b
Valerio <valerio.valletta@icloud.com>
2022-05-12 09:12:20 +0200

    Testing

--- Edumy/urls.py
+++ Edumy/urls.py
@@ -1,5 +1,5 @@
 """Edumy URL Configuration
-
+    Test
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
