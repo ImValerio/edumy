a784ffae4babe7aa11c5624635b75ce495e822b1
Valerio <valerio.valletta@icloud.com>
2022-05-16 21:22:36 +0200

    add comment on UserCreation

--- userAuth/models.py
+++ userAuth/models.py
@@ -8,6 +8,8 @@ from django import forms
 
 
 class UserCreation(UserCreationForm):
+    """
+
     username = forms.CharField()
     first_name = forms.CharField(max_length=32, help_text='First name')
     last_name = forms.CharField(max_length=32, help_text='Last name')
@@ -15,6 +17,7 @@ class UserCreation(UserCreationForm):
                              help_text='Enter a valid email address')
     password1 = forms.CharField()
     password2 = forms.CharField()
+"""
 
     class Meta(UserCreationForm.Meta):
         model = User
