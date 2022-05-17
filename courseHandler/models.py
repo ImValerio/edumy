b9a686d1b8fbdf631c9d59484b75fa6806fd158c
Valerio <valerio.valletta@icloud.com>
2022-05-17 22:07:48 +0200

    add Course model

--- courseHandler/models.py
+++ courseHandler/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+from userAuth.models import UserType
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(width_field=1920, height_field=1080)
+    duration = models.IntegerField()
+    price = models.IntegerField()
+    author = models.ForeignKey(UserType, related_name='course_user', on_delete=models.PROTECT)
+    is_active = models.BooleanField()
+    creation_date = models.DateField()
