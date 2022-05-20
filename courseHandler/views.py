43fd7a922334aede284ac9834d12014d07f49d61
giacomo <giacomo.zini30@gmail.com>
2022-05-20 23:07:47 +0200

    add course create form

--- courseHandler/views.py
+++ courseHandler/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from courseHandler.forms import CourseForm
+from courseHandler.models import Course
+
+
+class CourseCreate(CreateView):
+    model = Course
+    template_name = 'courseHandler/course/create.html'
+    #fields = ['title', 'description', 'category', 'price', 'creation_date']
+    success_url = reverse_lazy('hompage')
+    form_class = CourseForm
