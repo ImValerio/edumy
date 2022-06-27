dd1774d0c73ea6636e18dc606f7ce2c5c1ff5f5f
Valerio <valerio.valletta@icloud.com>
2022-06-27 22:07:55 +0200

    admin can handle all tables

--- courseHandler/admin.py
+++ courseHandler/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from courseHandler.models import Course, FollowCourse, Payment, Video
+from userAuth.models import UserType
+from userInteractions.models import Question, Answer, Review
+
+admin.site.register(Course)
+admin.site.register(FollowCourse)
+admin.site.register(Video)
+admin.site.register(Payment)
+
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Review)
+
+admin.site.register(UserType)
