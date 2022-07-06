1506c24fd0ea342e19a94c43b826eacabedb4a56
giacomo <giacomo.zini30@gmail.com>
2022-07-06 22:35:07 +0200

    fix recomandation homepage

--- Edumy/view.py
+++ Edumy/view.py
@@ -35,19 +35,6 @@ def recomandation(request):
             page_obj = paginator.get_page(page_number)
             context['page_obj'] = page_obj
             return render(request, "homepage.html", context)
-        else:
-            # query che prende tutte le recensioni che hanno almento un corso, le raggruppa per id e fa la media dei rating per quell'id
-            reviews_courses = Review.objects.select_related('course') \
-                                  .values('course').annotate(rating__avg=Avg('rating')).order_by("-rating__avg")[:10]
-            id_courses = [course for course in reviews_courses.values_list('course', flat=True)]
-            all_courses = Course.objects.filter(is_active='True')
-            courses_list = [course for course in all_courses if course.id in id_courses]
-            context['courseList'] = courses_list
-            paginator = Paginator(courses_list, 6)
-            page_number = request.GET.get('page')
-            page_obj = paginator.get_page(page_number)
-            context['page_obj'] = page_obj
-            return render(request, "homepage.html", context)
     #query che prende tutte le recensioni che hanno almento un corso, le raggruppa per id e fa la media dei rating per quell'id
     reviews_courses = Review.objects.select_related('course')\
         .values('course').annotate(rating__avg=Avg('rating')).order_by("-rating__avg")
